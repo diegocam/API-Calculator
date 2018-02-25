ed03517f94d363fdd56254625490a5e9c1433085
Diego Camacho <camachojcd@gmail.com>
2018-02-24 23:32:15 -0800

    Update sample request

--- index.php
+++ index.php
@@ -7,7 +7,12 @@ $json = '{"numbers":[1,23,2,3,4]}';
 $ch = curl_init($url);
 
 curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, array("request" => $json));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($json)
+    ]
+);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
 $curl_response = curl_exec($ch);
 curl_close($ch);
